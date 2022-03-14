3c28d258a1c0bf4f86e4c736a6ac5a606ce1d553
Lucas Alves <lucasvelosoalves@outlook.com>
2022-03-14 14:39:48 -0300

    Estrutura Final

---
+++ js/game.js
@@ -0,0 +1,138 @@
+let game = {
+
+    lockMode: false,
+    fristCard: null,
+    secondCard: null,
+
+    setCard: function (id) {
+
+        let card = this.cards.filter(card => card.id === id)[0];
+
+        if (card.flipped || this.lockMode) {
+            return false;
+        }
+
+        if (!this.fristCard) {
+            this.fristCard = card;
+            this.fristCard.flipped = true;
+            return true;
+        } else {
+            this.secondCard = card;
+            this.secondCard.flipped = true;
+            this.lockMode = true;
+            return true;
+        }
+
+    },
+
+    unFlipStartGame: function () {
+        let cardFlipped = document.getElementsByClassName('card');
+        this.lockMode = true;
+        
+            setTimeout(() => {
+                for (unflip of cardFlipped) {
+
+
+                    unflip.classList.remove('flip')
+                    
+                }
+            }, 1800)
+            
+            setTimeout(()=>{
+                this.lockMode = false;
+                
+            },2000)
+        
+        
+    },
+
+    checkMatch: function () {
+        if (!this.fristCard || !this.secondCard) {
+            return false
+        }
+        return this.fristCard.icon === this.secondCard.icon;
+    },
+
+    clearCards: function () {
+        this.fristCard = null;
+        this.secondCard = null;
+        this.lockMode = false;
+
+    },
+
+    unFlipCards: function () {
+        this.fristCard.flipped = false;
+        this.secondCard.flipped = false;
+        this.clearCards();
+    },
+
+    checkGameOver() {
+
+        return this.cards.filter(card => !card.flipped).length == 0
+
+    },
+
+    animals: [
+        'araraazul',
+        'tucano',
+        'araracaninde',
+        'araravermelha',
+        'sagui',
+        'onça',
+        'loboguara',
+        'jandaia',
+        'jiboia',
+        'veado',
+    ],
+
+    cards: null,
+
+    createCardsFromAnimals: function () {
+
+        this.cards = [];
+
+        this.animals.forEach((animal) => {
+            this.cards.push(this.createPairFromAnimals(animal));
+        })
+
+        this.cards = this.cards.flatMap(pair => pair)
+        this.shuffleCards();
+        return this.cards
+    },
+
+
+
+    createPairFromAnimals: function (animal) {
+        return [{
+            id: this.createIdWithAnimal(animal),
+            icon: animal,
+            flipped: false,
+        }, {
+            id: this.createIdWithAnimal(animal),
+            icon: animal,
+            flipped: false,
+        }]
+    },
+
+
+
+    createIdWithAnimal: function (animal) {
+        return animal + parseInt(Math.random() * 1000);
+    },
+
+
+    shuffleCards: function (cards) {
+        let currentIndex = this.cards.length;
+        let randomIndex = 0;
+
+        while (currentIndex !== 0) {
+
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]]
+        }
+    }
+
+
+}
---
+++ js/script.js
@@ -0,0 +1,128 @@
+const FRONT = "card-front"
+const BACK = "card-back"
+const CARD = "card"
+const ICON = "icon"
+
+const flipSound = document.getElementById('flipSound')
+
+
+
+
+
+
+function startGame() {
+
+    initializeCards(game.createCardsFromAnimals());
+    game.unFlipStartGame();
+    
+
+}
+
+function initializeCards(cards) {
+    let gameBoard = document.getElementById("gameBoard");
+    gameBoard.innerHTML = '';
+
+    game.cards.forEach(card => {
+
+        let cardElement = document.createElement('div')
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+        cardElement.classList.add('flip');
+
+
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard)
+        gameBoard.appendChild(cardElement);
+
+    })
+
+}
+
+function createCardContent(card, cardElement) {
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
+
+
+function createCardFace(face, card, element) {
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face)
+    if (face === FRONT) {
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = "./assets/" + card.icon + ".jpg";
+        cardElementFace.appendChild(iconElement);
+    } else {
+        cardElementFace.innerHTML = "?";
+    }
+    element.appendChild(cardElementFace)
+}
+
+
+function flipCard() {
+    if (game.lockMode === false) {
+        flipGameSound();
+        if (game.setCard(this.id)) {
+            this.classList.add("flip");
+            if (game.secondCard) {
+
+                if (game.checkMatch()) {
+
+                    game.clearCards();
+                    if (game.checkGameOver()) {
+                        let gameOverLayer = document.getElementById('gameOver');
+                        gameOverLayer.style.display = 'flex';
+                    }
+
+                } else {
+
+                    setTimeout(() => {
+
+                        let fristCardView = document.getElementById(game.fristCard.id);
+                        let SecondCardView = document.getElementById(game.secondCard.id);
+
+                        fristCardView.classList.remove('flip');
+                        SecondCardView.classList.remove('flip');
+                        game.unFlipCards();
+                    }, 1000);
+
+
+                }
+            }
+        }
+    }
+}
+
+
+function newGame() {
+    game.clearCards();
+    startGame();
+    let newGameLayer = document.getElementById('newGame')
+    newGameLayer.style.display = 'none'
+}
+
+function restart() {
+    game.clearCards();
+    startGame();
+    let gameOverLayer = document.getElementById('gameOver');
+    gameOverLayer.style.display = 'none';
+
+
+}
+
+function backMenu() {
+    let newGameLayer = document.getElementById('newGame')
+    newGameLayer.style.display = 'flex'
+    let gameOverLayer = document.getElementById('gameOver');
+    gameOverLayer.style.display = 'none';
+}
+
+function flipGameSound() {
+
+    flipSound.duration = 0.9
+    flipSound.volume = 0.1
+    flipSound.play()
+}
+
